f31e5dcc28967926ce521ce94fb9f1ef3d65b8ee
fleita selene <fleitaselene26@gmail.com>
2025-06-24 22:18:12 -0300

    feat: :sparkles: Exportacion de Metricas

--- backend/controllers/predict_controller.py
+++ backend/controllers/predict_controller.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 from backend.models.input_schema import DatosEntrada
 from backend.services.predict_service import predecir
-
+import json
 router = APIRouter()
 
 @router.post("/predict")
 def hacer_prediccion(datos: DatosEntrada):
     return predecir(datos)
+@router.get("/metricas")
+def obtener_metricas():
+    try:
+        with open("metricas.json", "r") as f:
+            metricas = json.load(f)
+        return metricas
+    except Exception as e:
+        return {"error": str(e)}
