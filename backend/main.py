7af3b4c98bb64707b28f4aa48e35a4f551841eba
fleita selene <fleitaselene26@gmail.com>
2025-06-25 03:40:27 -0300

    feat: :sparkles: Agregar, Editar y Implementar recursos de funcionalidad-Markdown

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from backend.controllers import predict_controller
+from fastapi.middleware.cors import CORSMiddleware
 
-# Inicialización de la app
 app = FastAPI()
 
-# Registro de rutas del controlador
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
+
 app.include_router(predict_controller.router)
 
 @app.get("/")
